66dd614c70d8e29b8a52fa86b3fa8706f1f6fd97
Carlos Anriquez <canriquez@gmail.com>
2020-02-26 17:44:54 -0300

    adding initial messages

--- bin/main.rb
+++ bin/main.rb
@@ -1,2 +1,35 @@
 #!/usr/bin/env ruby
-puts 'Hello world!'
+puts 'Main menu.'
+puts '--------------'
+puts '1. New game'
+puts '2. Exit Tic Tac Toe'
+
+input = gets.chomp
+
+
+puts 'Game menu.'
+puts '--------------'
+puts '1. Enter your move'
+puts '2. Exit game'
+
+input = gets.chomp
+
+
+puts "Your move is #{input}"
+puts '<method that display current board>'
+
+puts "\n\n\n <example>"
+puts '  0 |   |  '
+puts '    | X |  '
+puts "    |   | X \n\n\n"
+
+
+
+
+
+
+
+
+
+
+
