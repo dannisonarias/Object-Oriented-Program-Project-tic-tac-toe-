4eb517490fb6d071b5e236b88d13bd6221da9c5e
DannisonArias <dannisonarias@gmail.com>
2020-02-26 17:24:17 -0400

    Added some prompts to collect user input, and a loop

--- bin/main.rb
+++ bin/main.rb
@@ -1,35 +1,36 @@
 #!/usr/bin/env ruby
 puts 'Main menu.'
 puts '--------------'
+
 puts '1. New game'
-puts '2. Exit Tic Tac Toe'
+puts '2. Game Options'
+puts 'Type exit'
 
 input = gets.chomp
+exit if input == 'exit'
 
+# Loop for playing the game
+until %w[exit N n].include? input
+  puts '1. Enter your move'
 
-puts 'Game menu.'
-puts '--------------'
-puts '1. Enter your move'
-puts '2. Exit game'
+  puts '2. Exit game'
 
-input = gets.chomp
+  input = gets.chomp
 
+  puts "Your move is #{input}"
+  puts '<method that display current board>'
 
-puts "Your move is #{input}"
-puts '<method that display current board>'
+  puts "\n\n\n <example>"
+  puts '  0 |   |  '
+  puts '    | X |  '
+  puts "    |   | X \n\n\n"
 
-puts "\n\n\n <example>"
-puts '  0 |   |  '
-puts '    | X |  '
-puts "    |   | X \n\n\n"
+  puts 'game is over'
+  puts 'do you want to play again Y/N?'
+  input = gets.chomp # exit
+  p "good choice #{input}"
+end
 
-
-
-
-
-
-
-
-
-
-
+p 'Thank you for playing'
+p 'closing ....'
+exit
