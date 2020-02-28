95dcdf9c892ccbb97d2c1ecc7d9b075084005765
Carlos Anriquez <canriquez@gmail.com>
2020-02-28 17:21:14 -0300

    winnig evaluation done. game finished with one player hits a winning cobination

--- bin/main.rb
+++ bin/main.rb
@@ -2,8 +2,8 @@
 
 players = []
 allowed_moves = (1..9)
-pmoves = [[1,5,9,6],[6,4,3]]
-wining_moves = [[1,5,9],[7,5,3],[1,2,3],[4,5,6],[7,8,9],[1,4,7],[2,5,8],[3,6,9]]
+pmoves = [[],[]]
+winning_moves = [[1,5,9],[7,5,3],[1,2,3],[4,5,6],[7,8,9],[1,4,7],[2,5,8],[3,6,9]]
 
 
 puts 'Main menu.'
@@ -25,38 +25,46 @@ end
 
 # Loop for playing the game
 until %w[exit N n].include? input
-  
+winner = -1
+total_matches = 0
 
-#loop until winning or draw
+  #loop until winning or draw
+  while winner == -1 
 
-#looping through each player to get the move
-players.each_with_index do |name, index|
-  pinput = nil
-  
-  #loop until correct move
-  while pinput.nil?
-      puts "#{name}, please enter your move: #{allowed_moves}"
-      pinput = gets.chomp.to_i
-      if allowed_moves.any? {|move| pinput == move} && !pmoves[index].any?{|oldmove| oldmove == pinput}
-        puts "storing : #{pinput}, into array. Index is : #{index}"
+    #looping through each player to get the move
+    players.each_with_index do |name, index|
+      pinput = nil
+      
+      #loop until correct move
+      while pinput.nil? && winner == -1
+        puts "#{name}, #{pmoves[index]} please enter your move: #{allowed_moves}"
+        pinput = gets.chomp.to_i
+        # if below check if move valid and if is not already taken by any player
+        if allowed_moves.any? {|move| pinput == move} && !pmoves[0].any?{|oldmove| oldmove == pinput} && !pmoves[1].any?{|oldmove| oldmove == pinput}
+          puts "storing : #{pinput}, into array. Index is : #{index}"
           pmoves[index] << pinput 
           puts "stored : pmoves[#{index}]#{pmoves[index]}"
         else 
-          puts !pmoves[index].any?{|oldmove| oldmove == pinput} ? "#{name}, enter a valid move" : "#{name}, Move is takened!"
+          puts !pmoves[0].any?{|oldmove| oldmove == pinput} && !pmoves[1].any?{|oldmove| oldmove == pinput} ? "#{name}, enter a valid move" : "#{name}, Move is takened!"
           pinput = nil
+        end
+
+          #pmoves = [[1,5,9,6],[6,4,3]]
+              #winning_moves = [[1,5,9],[7,5,3],[1,2,3],[4,5,6],[7,8,9],[1,4,7],[2,5,8],[3,6,9]]
+        winning_moves.each do |win_array|                 #check each array in wining_moves
+          total_matches = win_array.map do |win_move|                 
+            pmoves[index].any?{|player_move| player_move == win_move} ? 1 : 0
+              #puts "win_array is : #{win_array}, checking now move #{player_move} against #{win_move}: iqual? : #{player_move == win_move}
+          end
+          puts "#{players[index]}, player. Moves so far: #{pmoves[index]}"
+          puts "total winning moves in sequences #{win_array} is : #{total_matches.inject(:+)} "
+          winner = index if total_matches.inject(:+) == 3
+        end
       end
-#pmoves = [[1,5,9,6],[6,4,3]]
-#wining_moves = [[1,5,9],[7,5,3],[1,2,3],[4,5,6],[7,8,9],[1,4,7],[2,5,8],[3,6,9]]
-      pmoves[index].each do |invimove|
-         winning_moves.inject do |total,array|
-          array.any? { |i| i == invimove }
-         end
-      end
+    end 
   end
 
-  #check for a winning player or draw 
-
-end
+puts "HEY. Congratulations #{players[winner]}, great match #{pmoves[winner]}"
 
 puts "#{players[0]} selected : #{pmoves[0][0]}, #{players[1]} selected : #{pmoves[1][0]}, "
 
